92441bbd8bcf276b23f3b0588adeb61389648aa9
abderrahim <abderrahimboudounit@gmail.com>
2023-01-31 23:05:13 +0100

    done with mariadb

--- srcs/requirements/mariadb/tools/database.sql
+++ srcs/requirements/mariadb/tools/database.sql
@@ -1,6 +1,16 @@
 CREATE DATABASE IF NOT EXISTS wpdb;
+
+--Creates a user named root with the password 123456789 and grants them access from any host
 CREATE USER 'root'@'%' IDENTIFIED BY '123456789';
+
+--Creates a user named wpuser with the password 123456789 and grants them access from any host
 CREATE USER 'wpuser'@'%' IDENTIFIED BY 'wp123456789';
+
+--Grants all privileges on the wpdb database to the wpuser use
 GRANT ALL ON wpdb.* TO 'wpuser'@'%';
+
+--Grants all privileges on all databases to the root user.
 GRANT ALL ON *.* TO 'root'@'%';
+
+--reloads the privileges from the grant tables in the MySQL database
 FLUSH PRIVILEGES;
