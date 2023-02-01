576539693d373c1ba912c0b3979cf8df3210ed0f
abderrahim <abderrahimboudounit@gmail.com>
2023-02-01 13:29:35 +0100

    adding env variables for mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 service mysql start
-mysql -u root -p'123456789' < /tmp/database.sql
+
+echo
+"CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;
+CREATE USER 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD';
+CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASSWORD';
+GRANT ALL ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
+GRANT ALL ON *.* TO 'root'@'%';
+FLUSH PRIVILEGES;" >> database.sql
+
+mysql -u root -p$ROOT_PASSWORD < database.sql
 
 kill `cat /var/run/mysqld/mysqld.pid`
 
